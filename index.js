3e00c750bcb862f3c0a53a7ca20aa1ecc0a84b2b
Yisrael Gurkow <yisrelg3@gmail.com>
2021-05-06 17:43:49 -0400

    commit tree fcc1a2426492b035c21efbfbe418aacbabe7b2bd

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Add your code here
+function submitData (name, email) {
+    return fetch ('http://localhost:3000/users',
+    {
+        method : 'POST',
+        headers : {
+            "Content-Type" : "application/json",
+            "Accept" : "application/json"
+        },
+        body : JSON.stringify({
+            name : "Steve",
+            email : "steve@steve.com"
+        })
+    })
+    .then(res => res.json())
+    .then((userObj) => {
+        const body = document.querySelector('body')
+        //console.log(usersArray)
+            const p = document.createElement('p')
+            p.textContent = userObj.id
+            body.append(p)
+    })
+    .catch((errorObj) => {
+        const body = document.querySelector('body')
+        const h3 = document.createElement('h3')
+        h3.textContent = errorObj.message
+        body.append(h3)
+    })
+}
+
+
+
+// fetch('http://localhost:3000/dogs',
+//     {
+//         method:'POST', 
+//         headers : {
+//             "Content-Type" : "application/json",
+//             "Accept" : "appliaction/json"
+//         },
+//         body : JSON.stringify({
+//             dogName : 'byron',
+//             dogBreed : "poodle"
+//         })
+//     }
+// )
+// .catch(function(error) {
+//     alert("Bad things! Ragnarők!");
+//     console.log(error.message);
+//   }); 
+
+//   const h1 = document.createElement('h1')
+//   h1.innerText= 'hi'
+//   const body = document.querySelector('body')
+//   console.log(h1)
+//   body.append(h1)
